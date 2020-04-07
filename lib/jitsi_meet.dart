f59549e62552a2bf7ed7487a5014029cc43edc13
Mark Eggenstein <>
2020-04-07 17:51:33 +0200

    Switched from minutes to seconds.

--- lib/jitsi_meet.dart
+++ lib/jitsi_meet.dart
@@ -14,7 +14,7 @@ class JitsiMeet {
   );
 
   static Future<JitsiMeetingResponse> joinMeeting(
-      JitsiMeetingOptions options, int durationInMinutes) async {
+      JitsiMeetingOptions options, int durationInSeconds) async {
     assert(options != null, "options are null");
     assert(options.room != null, "room is null");
     assert(options.room.trim().isNotEmpty, "room is empty");
@@ -39,7 +39,7 @@ class JitsiMeet {
           'videoMuted': options.videoMuted,
           'userDisplayName': options.userDisplayName,
           'userEmail': options.userEmail,
-          'durationInMinutes': durationInMinutes,
+          'durationInSeconds': durationInSeconds,
         })
         .then((message) =>
             JitsiMeetingResponse(isSuccess: true, message: message))
